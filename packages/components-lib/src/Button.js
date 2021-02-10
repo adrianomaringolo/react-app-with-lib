e7b90837ab307db68adadbd68a6c3c3785c665e3
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2021-02-09 22:44:43 -0300

    8 - create example component

---
+++ packages/components-lib/src/Button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background: blue;
+  color: #fff;
+  padding: 9px 36px;
+  border-radius: 4px;
+  font-size: 25px;
+  font-weight: 300;
+`;
+
+export default Button;
